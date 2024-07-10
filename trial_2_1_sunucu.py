591f2f133384d7c1c73aa7af3eeb78cb30194997
halit <dalha21@itu.edu.tr>
2024-07-11 00:03:33 +0300

    a few fixes were made

--- trial_2_1_sunucu.py
+++ trial_2_1_sunucu.py
@@ -3,40 +3,40 @@ import pandas as pd
 import pickle
 import time
 
-#veri okuma
+# Taking the data(cords_x and cords_y) from the csv file
 with open("files.csv", "r") as file:
     df=pd.read_csv(file)
 print(df)
-data=df.values.tolist()
+data=df.values.tolist()#convert the dataframe to a list
 file.close()
-#print(data)
 
+# Function to send data to the client
 def send_data(connection, veri):
     for sira, veri in enumerate(veri):
-        try:
+        try:#If the data is sent successfully, print the data
             print(f"Data: {sira} : {veri}")
             connection.sendall(pickle.dumps(veri))
             print("Data sent to the client")
-            time.sleep(0.5)
-        except:
+            time.sleep(0.5)#Data is sent every 0.5 seconds
+        except:#If the data is not sent successfully, print the error message
             print("Data could not be sent to the client")
             return False
     return True
 
-try:
+try:#If the server is created successfully, print the message
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
         server_socket.bind((socket.gethostbyname(socket.gethostname()), 5050))
         server_socket.listen()
         print("Server is listening...")
         connection, address=server_socket.accept()
-        with connection:
+        with connection:#If the connection is successful with the client the data is sent to the client
             while True:
-                #verileri sırasıyla gönder
+                #Send data in correct order to the client using the send_data function
                 if not send_data(connection, data):
                     break
-                #verileri ters sırayla gönder
+                #Send data in reverse order to the client using the send_data function
                 if not send_data(connection, data[::-1]):
                     break
-except:
+except:#If the server is not created successfully, print the error message
     print("Server could not be created")
     exit()
--- trial_2_2_client.py
+++ trial_2_2_client.py
@@ -4,28 +4,31 @@ import pickle
 import numpy as np
 from sklearn.linear_model import LinearRegression
 
-prediction = []
-errors = []  # To store error values
+prediction = [] #It stores the predicted points
+errors = []  #It stores the errors of the predictions
 
-# The function that processes the last 5 data points
+# The function that processes the last 5 data points and predicts the next point
 def process_last_5(data):
     # last_5[i][0]=cords x, last_5[i][1]=cords y
     last_5 = data[-5:]
-    # Fit a linear regression model to the last 5 data points
-    x = np.array([point[0] for point in last_5]).reshape(-1, 1)
-    y = np.array([point[1] for point in last_5])
-    model=LinearRegression()
-    model.fit(x, y)
     if len(last_5)>=2: #It needs at least 2 data points to predict the next point
+        # Fit a linear regression model to the last 5 data points
+        x = np.array([point[0] for point in last_5]).reshape(-1, 1)
+        y = np.array([point[1] for point in last_5])
+        # Assuming that given 5 points follow a linear trend, you can model this trend using linear regression and predict point 6
+        model=LinearRegression()
+        model.fit(x, y)
         x6=last_5[-1][0]+(last_5[-1][0]-last_5[-2][0])
         y6=model.predict([[x6]])
         global prediction, errors
+        # Store the prediction
         prediction.append((round(x6,2) ,round(y6[0],2)))
         print("Prediction: ", prediction[-6:])
-        #It shows the predicted point for the previous data and the point it predicts now
-        #Calculate and store the error
-        errors.append(abs((y6[0] - last_5[-1][1])/last_5[-1][1]*100))
-        print("Errors: ", errors[-6:])
+        # After making the initial guess take the actual value and record the errors after the error is calculated
+        if len(prediction)>=2: # It needs at least 2 predictions to calculate the error of the prediction
+            # Calculate and store the error
+            errors.append(round(abs((prediction[-2][1] - last_5[-1][1])/last_5[-1][1]*100),2))
+            print("Errors: ", errors[-6:])
     return last_5
 
 # The function that receives data from the server
@@ -36,11 +39,11 @@ def receive_data(client_socket):
     return pickle.loads(data)
 
 
-# Initial empty data for x and y
+# Initial empty data for cords_x and cords_y
 x = [0] * 5
 y = [0] * 5
 
-# Create a figure and axis
+# Create a figure and axis for data visualization
 fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 8))
 line, = ax1.plot(x, y)
 scatter = ax1.scatter(x, y, label='Data Points', color='b')
@@ -48,6 +51,9 @@ line_prediction, = ax1.plot([], [], color='r')
 scatter_prediction = ax1.scatter([], [], color='r', marker='o', label='Predictions Points')
 line_new_prediction, = ax1.plot([], [], color='g')
 scatter_new_prediction = ax1.scatter([], [], color='g', marker='x', label='New Prediction Point')
+ax1.set_xlabel('Cords X')
+ax1.set_ylabel('Cords Y')
+ax1.set_title('Real Time Data Visualization')
 
 # Create a figure and axis for error visualization
 error_line, = ax2.plot([], [], label='Prediction Error', color='m')
@@ -56,11 +62,8 @@ ax2.set_xlabel('Time')
 ax2.set_ylabel('Error')
 ax2.set_title('Prediction Error Over Time')
 
-ax1.set_xlabel('Cords X')
-ax1.set_ylabel('Cords Y')
-ax1.set_title('Real Time Data Visualization')
 
-# Update the data on the plot
+# Update the data and error visualization (making real-time)
 def update(frame):
     global x, y, scatter, prediction, scatter_prediction, scatter_new_prediction, errors, error_line, error_scatter
     x = [point[0] for point in frame]
@@ -68,7 +71,7 @@ def update(frame):
     line.set_ydata(y)
     line.set_xdata(x)
     scatter.set_offsets(np.c_[x, y])
-    if len(prediction) >= 1:
+    if len(prediction) >= 1: # It needs at least 1 prediction to visualize (drawing lines and points for predictions)
         pred_x = [point[0] for point in prediction[-6:]]
         pred_y = [point[1] for point in prediction[-6:]]
         line_prediction.set_ydata(pred_y[:-1])
@@ -77,7 +80,7 @@ def update(frame):
         scatter_new_prediction.set_offsets(np.c_[pred_x[-1:], pred_y[-1:]])
         line_new_prediction.set_ydata(pred_y[-1:])
         line_new_prediction.set_xdata(pred_x[-1:])
-    if len(errors) >= 1:
+    if len(errors) >= 1: # It needs at least 1 error to visualize (drawing lines and points for errors)
         error_line.set_ydata(errors[-6:])
         error_line.set_xdata(range(len(errors[-6:])))
         error_scatter.set_offsets(np.c_[range(len(errors[-6:])), errors[-6:]])
@@ -90,31 +93,31 @@ def update(frame):
 # The client receives the data from the server, processes the last 5 data points, and visualizes them.
 def get_data():
     my_data = []
-    server_ip = socket.gethostbyname(socket.gethostname())
+    server_ip = socket.gethostbyname(socket.gethostname()) # Get the IP address of the server
     server_port = 5050
-    try:
+    try:  #If the server is available, the client will be created
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
             client_socket.connect((server_ip, server_port))
             while True:
                 try:
-                    # Receives data from the server
+                    # Receives data from the server through the function receive_data
                     data = receive_data(client_socket)
                     my_data.append(data)
                     print("Data received from the server")
-                    # Processes the last 5 data points
+                    # Processes the last 5 data points through the function process_last_5
                     last_5 = process_last_5(my_data)
                     print("Last 5 data: ", last_5)
-                    # Visualize the last 5 data points
+                    # Visualize the last 5 data points and predictions through the function update
                     update(last_5)
-                    plt.pause(0.5)
-                except ConnectionError as e:
+                    plt.pause(0.5) 
+                except ConnectionError as e: #If the data cannot be received from the server, the client will be closed
                     print(e)
                     break
-    except ConnectionRefusedError:
+    except ConnectionRefusedError: #If the server is not available, the client will not be created
         print("Server is not available")
 
-last_5 = []
 plt.ion()  # Turn on interactive mode for matplotlib
-ax1.legend()
 get_data()
+ax1.legend()
+ax2.legend()
 plt.show()
